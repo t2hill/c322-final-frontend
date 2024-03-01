a2df8716ca4d3d75f40e5f558a389852c4a0d1ad
toohill.ted@gmail.com <toohill.ted@gmail.com>
2024-03-01 18:28:22 -0500

    Deploy on Render

--- script/myscript.js
+++ script/myscript.js
@@ -1,3 +1,8 @@
+// HOMEPAGE
+function clearCondiments() {
+    localStorage.setItem("condiments", JSON.stringify(""));
+    localStorage.setItem("beverage", "");
+}
 
 // BEVERAGES
 function setBeverage(buttonValue) {
@@ -48,41 +53,89 @@ function setElements() {
     console.log(localStorage.getItem("condiments"));
 }
 
-if (document.getElementById('orderMenu')) {
-    window.onload = setElements;
+async function addReceipt() {
+    // let host = "http://localhost:8080";
+    let host = "https://coffee-order-kgpi.onrender.com";
+    let message = "";
+    let orderData = {beverage: localStorage.getItem("beverage"), 
+                    condiments: JSON.parse(localStorage.getItem("condiments"))};
+    console.log(JSON.stringify(orderData));
+
+
+    // build request
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(orderData)
+    };
+
+    try {
+        let response = await fetch(host + "/orders", request);
+        if (response.status == 201) {
+                message = "The receipt was added to the system \n\n" + JSON.stringify(orderData);
+            } 
+    } catch (error) {
+        message = "The following error occurred:\n" + error + "\n\nPlease try again or contact the customer support team";
+    }
+    console.log(message);   
+    
 }
 
 // ORDER STATUS
-function getReceipt() {
-    let host = "http://localhost:8080";
-    let receipt = document.getElementById("getReceipt");
-    receipt.innerHTML = "";
-    let title = document.createElement("h2");
-    let titleVal = document.createTextNode("Order Placed:");
-    title.appendChild(titleVal);
-    receipt.appendChild(title);
+async function getReceipt() {
+    // let host = "http://localhost:8080";
+    let host = "https://coffee-order-kgpi.onrender.com";
+    let request = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    let url = host + "/orders";
+    try {
+        let response = await fetch(url, request);
+        let result = await response.json();
+        console.log(result);
 
-    let id = document.createElement("h2");
-    let idVal = document.createTextNode("Order id: 1");  // Would link to backend and grab this value
-    id.appendChild(idVal);
-    receipt.appendChild(id);
+        // Order Placed: 
+        let receipt = document.getElementById("getReceipt");
+        receipt.innerHTML = "";
+        let title = document.createElement("h2");
+        let titleVal = document.createTextNode("Order Placed:");
+        title.appendChild(titleVal);
+        receipt.appendChild(title);
 
-    let cond = document.createElement("h2");
-    let condiments = JSON.parse(localStorage.getItem("condiments"));
-    let condStr = localStorage.getItem("beverage") + " with ";
-    for (let c of condiments) {
-        condStr += c + " and ";
-    } 
-    let condVal = document.createTextNode(condStr.slice(0, -5) + "!")
-    cond.appendChild(condVal);
-    receipt.appendChild(cond);
+        let id = document.createElement("h2");
+        let idVal = document.createTextNode("Order id: " + result.id);  // Would link to backend and grab this value
+        id.appendChild(idVal);
+        receipt.appendChild(id);
 
-    let total = document.createElement("h2");
-    let totalVal = document.createTextNode("Total: $X");
-    total.appendChild(totalVal);
-    receipt.appendChild(total);
+        let cond = document.createElement("h2");
+        let condVal = document.createTextNode(result.description)
+        cond.appendChild(condVal);
+        receipt.appendChild(cond);
+
+        let total = document.createElement("h2");
+        let totalVal = document.createTextNode("Total: $" + result.cost);
+        total.appendChild(totalVal);
+        receipt.appendChild(total);
+
+    } catch (error) {
+        let message = "The following error occurred:\n" + error + "\n\nPlease try again or contact the customer support team";
+        alert(message);
+    }
 }
 
+
+// When to load pages
+if (document.getElementById('orderMenu')) {
+    window.onload = setElements;
+}
 if (document.getElementById('getReceipt')) {
     window.onload = getReceipt;
+}
+if (window.location.href === 'file:///C:/Users/toohi/Documents/C322/coffee-order-ui/index.html') {
+    localStorage.setItem("condiments", []);
 }
